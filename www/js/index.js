405df96441b36667039796a4771509f8b3620cf2
Seter Guo <seter@pku.edu.cn>
2015-11-12 02:10:26 +0800

    naive demo

---
+++ www/js/index.js
@@ -0,0 +1,39 @@
+$(document).ready(
+function () {
+	$('#image').mousedown(
+		function (event) {
+			var isMove = true;
+			var startX = event.pageX + $(window).width() - $('#image').offset().left - $('#image').width();
+			var startY = event.pageY + $(window).height() - $('#image').offset().top - $('#image').height();
+			$(document).mousemove(
+				function (event) {
+					if (isMove) {
+						var obj = $('#image');
+						obj.css({'right': startX - event.pageX, 'bottom': startY - event.pageY});
+					}
+				}
+			).mouseup(
+				function () {
+					isMove = false;
+					editor.focus();
+				}
+			);
+		}
+	);
+	$("#run").click(
+		function () {
+			$.ajax({
+				type: "POST",
+				url: "/run",
+				data: {'code': editor.getValue()},
+				cache: false,
+				success: function (data) {showSvg(data);},
+				error: function () {showSvg('<circle cx="150" cy="80" r="50" stroke="black" stroke-width="2" fill="gray"/>');}
+			});
+		}
+	);
+}
+);
+function showSvg(s) {
+	$("svg").html(s);
+}
